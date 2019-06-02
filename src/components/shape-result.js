06f9e02548d5577caa9a14e54d3e99a616bc1e21
Pankaj Parkar <parkarpankaj3@gmail.com>
2019-06-02 11:28:50 +0530

    Added dynamic shape text on resut page.

--- src/components/shape-result.js
+++ src/components/shape-result.js
@@ -2,7 +2,7 @@ import { StepCard } from "./step-card";
 
 const header = step => `Step ${step} - Your results`
 const content = (shape) => {
-    return `You have calculated the area of reactagle to ${shape.area}. Below is your result:	
+    return `You have calculated the area of ${shape.name.toLowerCase()} to ${shape.area}. Below is your result:	
     <h3>	
       The area is ${shape.area}
     </h3>`
@@ -16,7 +16,7 @@ export class ShapeResultComponent extends StepCard {
         const template = {
             header: header(currentStep.step),
             content: content(shape),
-            footer: footer
+            footer
         }
         super(template, currentStep);
     }
